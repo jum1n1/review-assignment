d377c9f608025d2f832d0434d4351cad5ce8b9aa
jumin <j2mie561@gmail.com>
2023-07-25 18:00:04 +0900

    init commit_2

---
+++ src/main/java/com/sparta/reviewassignment/ReviewAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.reviewassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewAssignmentApplication.class, args);
+    }
+
+}
