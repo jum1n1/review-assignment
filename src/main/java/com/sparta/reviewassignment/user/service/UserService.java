4564ceab82cb1c49ff7d25ab6d48eaa6e1e9378e
jumin <j2mie561@gmail.com>
2023-07-27 15:19:23 +0900

    회원가입 API 수정

--- src/main/java/com/sparta/reviewassignment/user/service/UserService.java
+++ src/main/java/com/sparta/reviewassignment/user/service/UserService.java
@@ -15,18 +15,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    // 닉네임과 같은 값이 포함된 경우 회원가입에 실패로 만들기 =>  service
-    // 비밀번호 확인은 비밀번호와 정확하게 일치하기 =>  service
-    // 데이터베이스에 존재하는 닉네임을 입력한 채 회원가입 버튼을 누른 경우 "중복된 닉네임입니다." =>  service
-    // 라는 에러메세지를 response에 포함하기
 
     public void signup(SignupRequestDto signupRequestDto) {
         User user = new User(signupRequestDto);
 
-        // 닉네임과 같은 값이 포함된 경우
-        if (user.getPassword().matches(user.getNickName())) {
+        // 닉네임과 같은 값이 포함된 경우 => 아에 같은 경우에만 체크함
+        if (user.getPassword().contains(user.getNickName())) {
             throw new IllegalArgumentException("비밀번호를 닉네임과 다르게 입력해주세요.");
-//            return ResponseEntity.badRequest().body(new MsgResponseDto("비밀번호를 닉네임과 다르게 입력해주세요."));
         }
 
         // 비밀번호 확인은 비밀번호와 정확하게 일치
